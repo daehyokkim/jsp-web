e73a20666e884027a7ad8ef5ff176f931bf8c108
test1 <kilcs12@hanmail.net>
2021-01-29 18:30:08 +0900

    add bbsVO

---
+++ BBS/src/bbs/bbs.java
@@ -0,0 +1,51 @@
+package bbs;
+
+public class bbs {
+
+	private int bbsNum;
+	private String bbsUserID;
+	private String bbsTitle;
+	private String bbsDate;
+	private String bbsContent;
+	private int bbsAvailable;
+	
+	public int getBbsNum() {
+		return bbsNum;
+	}
+	public void setBbsNum(int bbsNum) {
+		this.bbsNum = bbsNum;
+	}
+	public String getBbsUserID() {
+		return bbsUserID;
+	}
+	public void setBbsUserID(String bbsUserID) {
+		this.bbsUserID = bbsUserID;
+	}
+	public String getBbsTitle() {
+		return bbsTitle;
+	}
+	public void setBbsTitle(String bbsTitle) {
+		this.bbsTitle = bbsTitle;
+	}
+	public String getBbsDate() {
+		return bbsDate;
+	}
+	public void setBbsDate(String bbsDate) {
+		this.bbsDate = bbsDate;
+	}
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	public int getBbsAvailable() {
+		return bbsAvailable;
+	}
+	public void setBbsAvailable(int bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+	
+	
+	
+}
